63a58421d205dcb7609c2a39f002c6b22c53808a
argiad <a.s.mkrtchyan@gmail.com>
2020-04-22 08:53:41 -0700

    Initial Commit

---
+++ MMTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MMTest
+//
+//  Created by Artem Mkrtchyan on 4/22/20.
+//  Copyright © 2020 Artem Mkrtchyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
